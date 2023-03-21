2de05d9104b152ded31ff571a5ba931cf55f8574
shaopyshaozi <shaopengyuanspy@outlook.com>
2023-03-21 16:48:56 +0000

    update

--- game/final-version/game4.py
+++ game/final-version/game4.py
@@ -117,7 +117,7 @@ yellowNet_surface.blit(pygame.transform.scale(yellowNet, (button_width, button_h
 
 keys = [
     {'surface': redNet_surface, 'rect': redNet_surface.get_rect(centerx=200, centery=500), 'color1': RED, 'color2': (180, 0, 0), 'key': pygame.K_1, 'pressed': False, 'label': 'L\n'},
-    {'surface': greenNet_surface, 'rect': greenNet_surface.get_rect(centerx=400, centery=500), 'color1': GREEN, 'color2': (0, 180, 0), 'key': pygame.K_2, 'pressed': False}, 'label': 'U\n',
+    {'surface': greenNet_surface, 'rect': greenNet_surface.get_rect(centerx=400, centery=500), 'color1': GREEN, 'color2': (0, 180, 0), 'key': pygame.K_2, 'pressed': False, 'label': 'U\n'},
     {'surface': blueNet_surface, 'rect': blueNet_surface.get_rect(centerx=600, centery=500), 'color1': BLUE, 'color2': (0, 0, 180), 'key': pygame.K_3, 'pressed': False, 'label': 'D\n'},
     {'surface': yellowNet_surface, 'rect': yellowNet_surface.get_rect(centerx=800, centery=500), 'color1': YELLOW, 'color2': (180, 180, 0), 'key': pygame.K_4, 'pressed': False, 'label': 'R\n'}
 ]
